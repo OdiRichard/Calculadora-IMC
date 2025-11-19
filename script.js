8a7c0309fba03b7baa0320fe815227845ba4a964
OdiRichard <richardrauppodi@gmail.com>
2025-11-18 21:23:14 -0300

    Calculadora IMC & TMB 1.3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//IMC
+
 function cliqueAqui() {
   let nome = document.getElementById("nome").value;
   let peso = document.getElementById("peso").valueAsNumber;
@@ -33,10 +35,25 @@ function cliqueAqui() {
   console.log(valor);
 }
 
+//TMB
+
 function cliqueAqui1() {
   let nome = document.getElementById("nome1").value;
   let idade = document.getElementById("idade").valueAsNumber;
   let peso = document.getElementById("peso1").valueAsNumber;
   let altura = document.getElementById("altura1").valueAsNumber;
+  let sexo = document.getElementById("sexo").value;
   let resposta1 = document.getElementById("valor1");
+
+  let tmb = 0;
+
+  if (sexo === "M") {
+    //Homens
+    tmb = 88.36 + 13.4 * peso + 4.8 * altura - 5.7 * idade;
+  } else if (sexo === "F") {
+    //Mulheres
+    tmb = 447.6 + 9.2 * peso + 3.1 * altura - 4.3 * idade;
+  }
+
+  resposta1.innerText = `${nome}, sua TMB é: ${tmb.toFixed(2)} kcal por dia.`;
 }
